10c35f8ba7a2a1debac95ffa0a1f6c0eb785b976
alihanekiz <alihan.ekiz@outlook.de>
2025-01-18 16:02:16 +0100

    task: start with sticky buttons component

---
+++ components/sticky-buttons/index.tsx
@@ -0,0 +1,23 @@
+import "./index.scss";
+
+
+export default function StickyButtons() {
+    return (
+        <div className="sticky-buttons">
+            <div className="container">
+                <div className="row">
+                    <div className="col-6 bg-primary">
+                        <a href="#">
+                            test
+                        </a>
+                    </div>
+                    <div className="col-6">
+                        <a href="#">
+                            test
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
