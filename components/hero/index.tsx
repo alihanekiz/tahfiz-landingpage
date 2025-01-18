0a9b356501873c47cb2294eeefbe45b262ab8494
alihanekiz <alihan.ekiz@outlook.de>
2025-01-18 16:02:35 +0100

    task: update hero component

--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -2,16 +2,12 @@ import "./index.scss";
 
 export default function Hero() {
   return (
-    <div className="hero ">
-      <div className="container bg-primary">
-        <div className="row">
-          {/* here comes the contact details */}
-        </div>
-        <div className="row">
-          {/* here comes the headline + cta */}
-        </div>
-        <div className="row">
-          {/* here comes the services block */}
+    <div className="hero bg-primary">
+      <div className="content">
+        <h1>Hafizlik Merasimine hosgeldiniz.</h1>
+        <div className="d-flex my-3">
+          <a href="#" className="btn btn-primary">Program</a>
+          <a href="#" className="btn btn-secondary mx-2">Park imkanlari</a>
         </div>
       </div>
     </div>
