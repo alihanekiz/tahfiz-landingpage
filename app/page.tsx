3447ec91ec32b879ec623588622896efb5340f4c
alihanekiz <alihan.ekiz@outlook.de>
2025-01-18 16:02:42 +0100

    task: update home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 
 import Countdown from "@/components/countdown";
+import Hero from "@/components/hero";
+import StickyButtons from "@/components/sticky-buttons";
 
 export default function Home() {
   return (
     <div>
+      <Hero/>
       <Countdown eventDate={"2025-01-01T23:59:59"} />
+      <StickyButtons/>
     </div>
   );
 }
