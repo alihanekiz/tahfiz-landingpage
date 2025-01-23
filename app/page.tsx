273ff06a3156cc8621f64775fd7d353edb050a52
alihanekiz <alihan.ekiz@outlook.de>
2025-01-23 16:24:49 +0100

    chore: wip add border as svg

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Countdown from "@/components/countdown";
 import Hero from "@/components/hero";
 import ProgramBlock from "@/components/program-block";
 import StickyButtons from "@/components/sticky-buttons";
+import BorderPattern from '@/public/branding/border-pattern.svg';
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero/>
       <Countdown eventDate={"2025-01-01T23:59:59"} />
       <ProgramBlock/>
+      <BorderPattern/>
       <StickyButtons/>
     </div>
   );
