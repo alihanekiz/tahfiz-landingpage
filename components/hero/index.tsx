27fe1fb5644b9311a33097c464dfb6dfc6b9a42d
alihanekiz <alihan.ekiz@outlook.de>
2025-01-26 16:24:54 +0100

    chore: fix typo

--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -4,7 +4,7 @@ export default function Hero() {
   return (
     <div className="hero bg-primary">
       <div className="content">
-        <h1>Hafızlık Merasimine hosgeldiniz.</h1>
+        <h1>Hafızlık Merasimine hoşgeldiniz.</h1>
         <div className="d-flex my-3">
           <a href="#program" className="btn btn-primary">Program</a>
           <a href="#" className="btn btn-secondary mx-2">Park imkanları</a>
